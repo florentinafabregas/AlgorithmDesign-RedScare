406511cceabb77eae6f1d148b9f97a6d41fb2c0e
Ondrej Kozanyi <ondrej.kozanyi@gmail.com>
2025-11-27 20:33:35 +0100

    added comments to the None solution and updated results

--- code/None.py
+++ code/None.py
@@ -6,12 +6,19 @@ import os
 
 
 def solveNone(g):
+    """
+    Implementation of the None problem solution through BFS
+    
+    :param g: graph instance object
+    """
     if g.s == g.t:
         return 0
 
+    # trivial case where s and t are directly connected
     if (g.t in g.neighbors(g.s)):
         return 1
 
+    # nodes s and t do not fall under the red-avoidance constraint
     r_pruned = g.red - {g.s, g.t}
 
     distances = {g.s: 0}
@@ -24,15 +31,18 @@ def solveNone(g):
         curr = queue.popleft()
 
         for next in g.neighbors(curr):
+            # we avoid any internal red nodes
             if next in r_pruned:
                 continue
 
             if next not in distances:
                 distances[next] = distances[curr] + 1
+                # if node t is reached, BSF found the shortest valid path
                 if next == g.t:
                     return distances[next]
                 queue.append(next)
     
+    # no such path exists
     return -1
 
 
@@ -41,7 +51,6 @@ if __name__ == "__main__":
     graph_files = sorted(glob.glob(f"{data_dir}/*.txt"))
 
     results = []
-    # graph_file = "../red-scare/data/G-ex.txt"
     for filepath in graph_files:
         g = GraphParser.from_file(filepath)
 
@@ -55,7 +64,7 @@ if __name__ == "__main__":
             results.append((name, "ERROR", str(e)))
             print(f"{name}: ERROR — {e}")
 
-    # write CSV
+    # write CSV results
     csv_file = "../results/none_results.csv"
     with open(csv_file, "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
